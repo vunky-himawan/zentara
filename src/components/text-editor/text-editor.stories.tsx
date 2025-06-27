48788a8bec9e8da21c62b5abad3bde4cee4a4154
Vunky Himawan <tingkerbeii44@gmail.com>
2025-06-27 22:33:44 +0700

    refactor: streamline CI/CD linting step and improve TextEditor and Sidebar stories formatting

--- src/components/text-editor/text-editor.stories.tsx
+++ src/components/text-editor/text-editor.stories.tsx
@@ -2,18 +2,18 @@ import type { Meta, StoryObj } from "@storybook/react/*";
 import { TextEditor } from ".";
 
 const meta: Meta<typeof TextEditor> = {
-  title: "@zentara/TextEditor",
-  component: TextEditor,
-  argTypes: {
-    menuProps: {
-      control: { type: "object" },
-    },
-  },
+	title: "@zentara/TextEditor",
+	component: TextEditor,
+	argTypes: {
+		menuProps: {
+			control: { type: "object" },
+		},
+	},
 };
 export default meta;
 
 type Story = StoryObj<typeof TextEditor>;
 
 export const Default: Story = {
-  args: {},
+	args: {},
 };
